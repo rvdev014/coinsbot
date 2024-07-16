f3a9eb83533c3b7e918cb889b7bd7328e8d1f3c5
murodovCorp <murodov.corp@gmail.com>
2024-07-17 01:00:53 +0500

    udpate last v

--- src/google/init.ts
+++ src/google/init.ts
@@ -2,7 +2,7 @@
 export const initGA = () => {
     const script = document.createElement('script');
     script.async = true;
-    script.src = `https://www.googletagmanager.com/gtag/js?id=G-XXXXXXXXXX`; // Замените на ваш Tracking ID
+    script.src = `https://www.googletagmanager.com/gtag/js?id=G-PY4TZRR9PB`; // Замените на ваш Tracking ID
     document.head.appendChild(script);
 
     // eslint-disable-next-line @typescript-eslint/ban-ts-comment
